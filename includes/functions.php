88539e267d1412b879c30e0c20ca4af005d133bd
khenryjoe <joseph.ketterer@gmail.com>
2019-05-17 19:10:30 +0100

    and switch case

--- includes/functions.php
+++ includes/functions.php
@@ -8,24 +8,40 @@ function formatMoney($number) {
 
 function printAuthor($authors){
     $output = '';
-    if(count($authors) == 1){
+    switch(true){
+
+    case (count($authors) == 1):
     $output= '<p>'. htmlentities($authors[0]). '<p>'. PHP_EOL;
-    }
-    else if (count($authors) == 2){
-        $output= '<p>'. htmlentities($authors[0]). ' and '. htmlentities($authors[1]). '<p>'. PHP_EOL;
-    }
-    else{
+    break;
+    
+    case (count($authors) == 2):
+        $output= '<p>'. htmlentities($authors[0]). ' and '. htmlentities($authors[1]). '<p>';
+        break;
+    
+    default:   
     $finalauthor = array_reverse($authors);
-    $output.= '<p>'. $authors[0];
+    $output.=  '<p>' . $authors[0];
     array_shift($authors);
     array_pop($authors);
     foreach ($authors as $key => $author) {
         $output.=', ' . htmlentities($author);
     }
     
-    $output.= ' and '.  htmlentities($finalauthor[0]). '</p>'. PHP_EOL;
+    $output .=  ' and '.  htmlentities($finalauthor[0]). '</p>'. PHP_EOL;
 }
     return $output;
 }
 
-?>
+function printBook($title, $isbn, $price, $authors){
+
+
+    
+    $output = '<table> <tr> <td>'. $title . '</td> <td>' . $isbn
+    . '</td> <td>' .  formatMoney($price) .
+    '</td>   <td>' . printAuthor($authors) . '</td> </tr> </table>';
+    
+    return $output;  
+}
+
+
+    ?>           
--- views/books.php
+++ views/books.php
@@ -1,3 +1,4 @@
 <?php
 $content .= '<h1>Welcome to the books page</h1>';
+$content .= printBook('Prioritizing Web Usability','978-0321350312', 24.99, array('Jakob Nielsen','steve', 'Hoa Loranger'));
  ?>
