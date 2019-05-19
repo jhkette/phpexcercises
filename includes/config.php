d9d2d0fdb8593a2045e7a31df2547b56f0ead1b7
khenryjoe <joseph.ketterer@gmail.com>
2019-05-19 19:44:42 +0100

    tidying

--- includes/config.php
+++ includes/config.php
@@ -1,12 +1,5 @@
 <?php
 
-// $link = mysqli_connect( 'localhost',
-// 'root',
-// // Pass the DB credentials to the
-//  '',
-// // connect function, store handle
-// 'test'
-// );
 define('DB_HOST', 'localhost' );
 define('DB_USER', 'root');
 define('DB_PASS', '');
